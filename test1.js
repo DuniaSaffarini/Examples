210ea2c14517a95d0a342a19e7cb57ba1ad8e989
DuniaSaffarini <35115614+DuniaSaffarini@users.noreply.github.com>
2019-10-08 10:48:27 +0300

    Delete test1.js

--- test1.js
+++
@@ -1,7 +0,0 @@
-function  teststyle(){
-    ///  document.getElementById("demo").innerHTML = "Hello JavaScript";
-    document.getElementById("demo").style.fontSize = "35px";
-    document.getElementById("test").style.display = "block";
-    document.getElementById("test").style.color = "pink";
-
-}
