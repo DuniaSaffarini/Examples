08eb54b5798d92a958bbea37633fd1f6ccfd697b
DuniaSaffarini <35115614+DuniaSaffarini@users.noreply.github.com>
2019-10-08 10:23:56 +0300

    Update jquery.shuffle-string.js

--- jquery.shuffle-string.js
+++ jquery.shuffle-string.js
@@ -1,8 +1,24 @@
 (function ($) {
-$.fn.shuffleString=function(){
+$.fn.shuffleeString=function(){
 
     return this.each(function () {
-        
+      $(this).text(shuffelString($(this).text));// this refer to h2
+
+        function shuffelString(str) {
+            var array=str+''.split("");
+            for(var i=array.length;i>0;i--){
+
+                var value=Math.floor((Math.random()*(i+1)));
+                var temp=array[i];
+                array[i]=array[value];
+                array[value]=temp;
+
+            //    var array=str[array.length-1];
+
+            }
+            return array.join("");
+        }
+
     })
 
 }
